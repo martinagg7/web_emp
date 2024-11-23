adcc15e81e4519fd5908c745e1124515d8cabe78
Martina Garcia <martinagg@myuax.com>
2024-11-23 19:34:48 +0100

    blog-avanzando

--- webempresarial/blog/views.py
+++ webempresarial/blog/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
-from .models import Post
-
-# Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Category
 
 def blog(request):
     posts = Post.objects.all()  # Obtén todos los posts
     return render(request, "blog/blog.html", {'posts': posts})
+
+def category(request, category_id):
+    category = get_object_or_404(Category, id=category_id)  # Obtén la categoría o lanza 404
+    return render(request, "blog/category.html", {'category': category})
