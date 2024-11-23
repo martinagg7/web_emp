cd4d7cd1a9a1744f589188cad1214cc0d8672549
Martina Garcia <martinagg@myuax.com>
2024-11-23 19:54:16 +0100

    category-no funciona

--- webempresarial/blog/views.py
+++ webempresarial/blog/views.py
@@ -2,12 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Category
 
 def blog(request):
-
     posts = Post.objects.all()
     return render(request, "blog/blog.html", {'posts': posts})
 
 
 def category(request, category_id):
-
     category = get_object_or_404(Category, id=category_id)
-    return render(request, "blog/category.html", {'category': category})
+    posts = category.posts.all()  # Accede a los posts relacionados con el related_name
+    return render(request, "blog/category.html", {'category': category, 'posts': posts})
