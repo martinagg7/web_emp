f7fba1f697e8518f7e3ebd4088a45795fe7c860e
Martina Garcia <martinagg@myuax.com>
2024-11-23 22:32:31 +0100

    models-admin

--- webempresarial/pages/admin.py
+++ webempresarial/pages/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Page
 # Register your models here.
+
+from .models import Page
+class PageAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Page, PageAdmin)
