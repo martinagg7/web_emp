b7f39b3085131dbf8edc533a36415dd3f1c0956a
Martina Garcia <martinagg@myuax.com>
2024-11-23 19:08:39 +0100

    ajustando blog

--- webempresarial/blog/views.py
+++ webempresarial/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+from django.shortcuts import render
+
+def blog(request):
+    posts = Post.objects.all()  # Obtén todos los posts
+    return render(request, "blog/blog.html", {'posts': posts})
--- webempresarial/core/urls.py
+++ webempresarial/core/urls.py
@@ -8,5 +8,5 @@ urlpatterns=[
     path('sample/',views.sample,name='sample'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
-    path('blog/', views.blog, name='blog'),
+    
 ]
