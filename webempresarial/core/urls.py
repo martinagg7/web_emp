33685758c9144cbdcf9a14ef22e9e805218705d8
Martina Garcia <martinagg@myuax.com>
2024-11-23 18:24:07 +0100

    pruba-services

--- webempresarial/core/urls.py
+++ webempresarial/core/urls.py
@@ -6,7 +6,6 @@ urlpatterns=[
     path('products/',views.products,name='products'),
     path('store/', views.store, name='store'), 
     path('sample/',views.sample,name='sample'),
-    path('services/',views.services,name='services'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('blog/', views.blog, name='blog'),
--- webempresarial/webempresarial/urls.py
+++ webempresarial/webempresarial/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 urlpatterns = [
-    path('', include('core.urls')), 
+    path('', include('core.urls')),
+    path('services/',include('services.urls')) ,
     path('admin/', admin.site.urls),
 ]
 
