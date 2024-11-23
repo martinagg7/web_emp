0fa72f8ec4f5bf857a193a28ef8ec15bf1aa06c4
Martina Garcia <martinagg@myuax.com>
2024-11-23 23:14:19 +0100

    pages-admin

--- webempresarial/pages/admin.py
+++ webempresarial/pages/admin.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 from .models import Page
 # Register your models here.
 
-from .models import Page
 class PageAdmin(admin.ModelAdmin):
-    pass
-
+    readonly_fields = ('created', 'updated')
+    list_display = ('title', 'order')
 admin.site.register(Page, PageAdmin)
