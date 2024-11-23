83f6aea8f5a81a612c7a17d40ce9f6bd3577ac82
Martina Garcia <martinagg@myuax.com>
2024-11-23 19:45:16 +0100

    template-category.blog

--- webempresarial/blog/views.py
+++ webempresarial/blog/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Category
 
 def blog(request):
-    posts = Post.objects.all()  # Obtén todos los posts
+
+    posts = Post.objects.all()
     return render(request, "blog/blog.html", {'posts': posts})
 
+
 def category(request, category_id):
-    category = get_object_or_404(Category, id=category_id)  # Obtén la categoría o lanza 404
+
+    category = get_object_or_404(Category, id=category_id)
     return render(request, "blog/category.html", {'category': category})
