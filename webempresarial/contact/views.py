f6bd4d8f74030888995eb99ec539271ef06d0e61
Martina Garcia <martinagg@myuax.com>
2024-11-24 13:15:07 +0100

    contact-form

--- webempresarial/contact/views.py
+++ webempresarial/contact/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from .forms import ContactForm  # Asegúrate de tener este formulario definido en forms.py
 
-def contact(request):
-    contact_form = ContactForm()
-    return render(request, 'contact/contact.html', {'form': contact_form})
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from .forms import ContactForm
+
+def contacto(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)  
+        if form.is_valid():  # verif formulario es válido
+            
+            name = request.POST.get("name", "")
+            email = request.POST.get("email", "")
+            content = request.POST.get("content", "")
+          
+            print(f"Nombre: {name}, Email: {email}, Contenido: {content}")
+
+
+            return redirect(f"{reverse('contacto')}?ok=True")
+    else:
+        form = ContactForm()  
+
+    # Renderizamos el template
+    return render(request, "core/contacto.html", {"form": form})
