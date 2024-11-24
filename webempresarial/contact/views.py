c428c6d39f75d29660bed22925884e0c58708c8e
Martina Garcia <martinagg@myuax.com>
2024-11-24 13:18:00 +0100

    contac-prueba

--- webempresarial/contact/views.py
+++ webempresarial/contact/views.py
@@ -1,25 +1,23 @@
-from django.shortcuts import render
-from .forms import ContactForm  # Asegúrate de tener este formulario definido en forms.py
-
 from django.shortcuts import render, redirect
-from django.urls import reverse
 from .forms import ContactForm
+from django.urls import reverse
 
-def contacto(request):
+def contact(request):
     if request.method == "POST":
-        form = ContactForm(request.POST)  
-        if form.is_valid():  # verif formulario es válido
-            
+        form = ContactForm(request.POST)
+        if form.is_valid():  # Verificar si el formulario es válido
+            # Recuperar los datos del formulario de forma segura
             name = request.POST.get("name", "")
             email = request.POST.get("email", "")
             content = request.POST.get("content", "")
-          
+            
+            # Aquí puedes hacer lo que quieras con los datos, como enviar un email
             print(f"Nombre: {name}, Email: {email}, Contenido: {content}")
 
-
-            return redirect(f"{reverse('contacto')}?ok=True")
+            # Redirigir con un parámetro GET 'ok' para mostrar un mensaje de éxito
+            return redirect(f"{reverse('contact')}?ok=True")
     else:
-        form = ContactForm()  
+        form = ContactForm()  # Si la solicitud es GET, mostramos el formulario vacío
 
     # Renderizamos el template
-    return render(request, "core/contacto.html", {"form": form})
+    return render(request, "contact/contact.html", {"form": form})
