955e4a7970ba6d018f112a13cf111a134648cf9e
Martina Garcia <martinagg@myuax.com>
2024-11-24 13:28:34 +0100

    contact-cambios formato

--- webempresarial/contact/forms.py
+++ webempresarial/contact/forms.py
@@ -1,5 +1,9 @@
 from django import forms
+
 class ContactForm(forms.Form):
-    name = forms.CharField(label="Nombre", required=True)
-    email = forms.EmailField(label="Email", required=True)
-    content = forms.CharField(label="Contenido", required=True, widget=forms.Textarea())
+    name = forms.CharField(label='Nombre', min_length=3, max_length=100, required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'Escribe tu nombre', 'class': 'form-control'}))
+    email = forms.EmailField(label='Email', min_length=3, max_length=100, required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'Escribe tu email', 'class': 'form-control'}))
+    content = forms.CharField(label='Mensaje', min_length=10, max_length=1000, required=True,
+        widget=forms.Textarea(attrs={'placeholder': 'Escribe tu mensaje', 'rows': 3, 'class': 'form-control'}))
